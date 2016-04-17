717785f920ff58eb3caaf14e3ca4433bcf845e62
David Tran <davidlamt@gmail.com>
2016-04-17 14:57:34 -0700

    Add Foundation to project

--- app/app.jsx
+++ app/app.jsx
@@ -8,6 +8,9 @@ import Main from 'Main';
 import MainMenu from 'MainMenu';
 import Registration from 'Registration';
 
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
 ReactDOM.render(
     <Router history={ hashHistory }>
         <Route path='/' component={ Main }>
