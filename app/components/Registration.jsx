53478e1073acec02f27707ac3526bf035dceca79
David Tran <davidlamt@gmail.com>
2016-04-22 12:22:55 -0700

    Implement registration page layout without submit functionality

--- app/components/Registration.jsx
+++ app/components/Registration.jsx
@@ -1,27 +1,46 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
+import Footer from 'Footer';
 
 class Registration extends Component {
     render() {
         return (
-            <div className='registration-page'>
-                <div className='container vertical-center'>
-                    <div className='text-center'>
-                        <Link className='platform-name' to='/'>AntFinder</Link>
-                    </div>
-                    <div className='row'>
-                        <div className='well col-md-6 col-md-offset-3'>
-                            <h2 className='text-center'>Sign Up</h2>
-                            <form>
-                                <div className='form-group'>
-                                    <input type='text' className='form-control' placeholder='First Name'/>
+            <div>
+                <div className='registration-page'>
+                    <div className='container vertical-center'>
+                        <div className='row'>
+                            <div className='col-md-6 col-md-offset-3 text-center'>
+                                <Link className='platform-name' to='/'>AntFinder</Link>
+                                <div className='well'>
+                                    <h2 className='register-header'>Sign Up</h2>
+                                    <form>
+                                        <div className='form-group'>
+                                            <input type='text' className='form-control' placeholder='First Name'/>
+                                        </div>
+                                        <div className='form-group'>
+                                            <input type='text' className='form-control' placeholder='Last Name'/>
+                                        </div>
+                                        <div className='form-group'>
+                                            <input type='text' className='form-control' placeholder='Email'/>
+                                        </div>
+                                        <div className='form-group'>
+                                            <input type='text' className='form-control' placeholder='Username'/>
+                                        </div>
+                                        <div className='form-group'>
+                                            <input type='text' className='form-control' placeholder='Password' />
+                                        </div>
+                                        <div className='form-group'>
+                                            <input type='text' className='form-control' placeholder='Confirm Password' />
+                                        </div>
+                                        <button type='submit' className='btn btn-primary btn-block'>Register</button>
+                                    </form>
                                 </div>
-                            </form>
+                            </div>
                         </div>
                     </div>
                 </div>
-
+                <Footer />
             </div>
         );
     }
