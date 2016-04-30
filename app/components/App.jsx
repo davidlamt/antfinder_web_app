72c72780d0452f5bd21f20c7f2ac291c75ac6556
David Tran <davidlamt@gmail.com>
2016-04-30 14:58:26 -0700

    Redirect unauthorized users back to home screen

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,6 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+import { authenticateUser } from '../actions/index';
 
 class App extends Component {
+    static contextTypes = {
+        router: PropTypes.object
+    };
+
+    componentWillMount() {
+        this.props.authenticateUser().then(response => {
+            if (response.error) this.context.router.push('/');
+        });
+    }
+
     render() {
         return (
             <div>
@@ -10,4 +23,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default connect(null, { authenticateUser })(App);
