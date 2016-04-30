9c7473d7a3dd42f52840357b887d1427b6c0f8f7
David Tran <davidlamt@gmail.com>
2016-04-29 20:10:28 -0700

    Implement registration using redux-form without validation

--- app/components/Registration.jsx
+++ app/components/Registration.jsx
@@ -2,6 +2,8 @@ import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { Link } from 'react-router';
 
+import { createUser } from '../actions/index';
+
 import Footer from 'Footer';
 
 class Registration extends Component {
@@ -9,7 +11,15 @@ class Registration extends Component {
         router: PropTypes.object
     };
 
+    onSubmit(props) {
+        this.props.createUser(props).then(() => {
+            console.log('user created!');
+        });
+    }
+
     render() {
+        const { fields: { firstName, lastName, email, username, password, confirmPassword }, handleSubmit } = this.props;
+
         return (
             <div>
                 <div className='registration-page'>
@@ -19,24 +29,24 @@ class Registration extends Component {
                                 <Link className='platform-name' to='/'>AntFinder</Link>
                                 <div className='well'>
                                     <h2 className='register-header'>Sign Up</h2>
-                                    <form>
+                                    <form onSubmit={ handleSubmit(this.onSubmit.bind(this)) }>
                                         <div className='form-group'>
-                                            <input type='text' className='form-control' placeholder='First Name'/>
+                                            <input type='text' className='form-control' placeholder='First Name' { ...firstName } />
                                         </div>
                                         <div className='form-group'>
-                                            <input type='text' className='form-control' placeholder='Last Name'/>
+                                            <input type='text' className='form-control' placeholder='Last Name' { ...lastName } />
                                         </div>
                                         <div className='form-group'>
-                                            <input type='text' className='form-control' placeholder='Email'/>
+                                            <input type='text' className='form-control' placeholder='Email' { ...email } />
                                         </div>
                                         <div className='form-group'>
-                                            <input type='text' className='form-control' placeholder='Username'/>
+                                            <input type='text' className='form-control' placeholder='Username' { ...username } />
                                         </div>
                                         <div className='form-group'>
-                                            <input type='password' className='form-control' placeholder='Password' />
+                                            <input type='password' className='form-control' placeholder='Password' { ...password } />
                                         </div>
                                         <div className='form-group'>
-                                            <input type='password' className='form-control' placeholder='Confirm Password' />
+                                            <input type='password' className='form-control' placeholder='Confirm Password' { ...confirmPassword } />
                                         </div>
                                         <button type='submit' className='btn btn-primary btn-block'>Register</button>
                                     </form>
@@ -51,4 +61,14 @@ class Registration extends Component {
     }
 }
 
-export default Registration;
+const validate = values => {
+    const errors = {};
+
+    return errors;
+};
+
+export default reduxForm({
+    form: 'RegistrationForm',
+    fields: ['firstName', 'lastName', 'email', 'username', 'password', 'confirmPassword'],
+    validate
+}, null, { createUser })(Registration);
