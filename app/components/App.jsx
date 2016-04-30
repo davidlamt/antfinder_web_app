c6a8e1953c7e3d077fa1986b32e161c07f351920
David Tran <davidlamt@gmail.com>
2016-04-30 15:47:24 -0700

    Add application side navbar menu

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
 
 import { authenticateUser } from '../actions/index';
 
@@ -16,16 +18,33 @@ class App extends Component {
 
     render() {
         return (
-            <div className='container-fluid'>
-                <div className='row'>
-                    <div className='col-sm-3 col-lg-2'>
-                        <nav className='navbar navbar-default navbar-fixed-side'>
-                        </nav>
-                    </div>
-                    <div className='col-sm-9 col-lg-10'>
-                        { this.props.children }
-                    </div>
+            <div className="container-fluid app-nav">
+                <div className="row">
+                <div className="col-sm-3 col-lg-2">
+                    <nav className="navbar navbar-inverse navbar-fixed-side">
+                        <div className='navbar-header'>
+                            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-page-menu" aria-expanded="false">
+                                <span className="sr-only">Toggle navigation</span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+                            <div className='navbar-brand'>AntFinder</div>
+                        </div>
+                        <div className='collapse navbar-collapse' id='app-page-menu'>
+                            <ul className='nav navbar-nav'>
+                                <li><Link activeClassName='active' to='app'>Dashboard</Link></li>
+                                <li><Link activeClassName='active' to='listings'>Listings</Link></li>
+                                <li><Link activeClassName='active' to='account'>Account</Link></li>
+                                <li><Link activeClassName='active' to='logout'>Logout</Link></li>
+                            </ul>
+                        </div>
+                    </nav>
                 </div>
+                <div className="col-sm-9 col-lg-10">
+                    { this.props.children }
+                </div>
+            </div>
             </div>
         );
     }
