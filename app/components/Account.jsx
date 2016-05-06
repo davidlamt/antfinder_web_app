5fb8bbbdbabc537770d084d5143e1137679d80db
David Tran <davidlamt@gmail.com>
2016-05-05 18:32:00 -0700

    Add the moment.js library and format the creation time on the account page

--- app/components/Account.jsx
+++ app/components/Account.jsx
@@ -1,13 +1,72 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { getUserInfo } from '../actions/index';
 
 class Account extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
+        const { firstName, lastName, email, username, status, createdAt } = this.props.user;
+
+        const determineAgeOfAccount = createdAt => moment().diff(moment(createdAt), 'days');
+
         return (
             <div>
-                <h1>Account Page</h1>
+                <h1 className='text-center menu-page-heading'>Account</h1>
+                <div className='row'>
+                    <div className='form-group col-md-4'>
+                        <label>First Name</label>
+                        <input className='form-control' value={ firstName } disabled></input>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='form-group col-md-4'>
+                        <label>Last Name</label>
+                        <input className='form-control' value={ lastName } disabled></input>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='form-group col-md-4'>
+                        <label>Email</label>
+                        <input className='form-control' value={ email } disabled></input>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='form-group col-md-4'>
+                        <label>Username</label>
+                        <input className='form-control' value={ username } disabled></input>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='form-group col-md-4'>
+                        <label>Account Status</label>
+                        <input className='form-control' value={ status } disabled></input>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='form-group col-md-12'>
+                        <label>Creation</label>
+                        <p>Your account was created { determineAgeOfAccount(createdAt) } days ago!</p>
+                    </div>
+                </div>
+
+                <h2>Change Password</h2>
+                <div className='row'>
+                    <div className='form-group col-md-12'>
+                        <label>Creation</label>
+                        <p>Your account was created { determineAgeOfAccount(createdAt) } days ago!</p>
+                    </div>
+                </div>
             </div>
         );
     }
 }
 
-export default Account;
+const mapStateToProps = state => {
+    return { user: state.users.info };
+}
+
+export default connect(mapStateToProps, { getUserInfo })(Account);
