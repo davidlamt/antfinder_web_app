aa0dbfc80f631ee21c3090fd45dd2cccbb97cd4a
David Tran <davidlamt@gmail.com>
2016-05-21 11:50:01 -0700

    Style spinner on listing view

--- app/components/Listing.jsx
+++ app/components/Listing.jsx
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 
 class Listing extends Component {
+    componentWillMount() {
+        console.log(this.props.params.listingID);
+    }
+
     render() {
+        if (!this.props.listing) return (
+            <div className='loading-spinner'>
+                <div className='text-center vertical-center'>
+                    <i className="fa fa-spinner fa-pulse fa-3x fa-fw margin-bottom"></i>
+                    <span className="sr-only">Loading...</span>
+                </div>
+            </div>
+        );
+
         return (
             <div>
                 Listing
