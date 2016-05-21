7ebf04dba0b0cf702aaf2914f388584c00db8abf
David Tran <davidlamt@gmail.com>
2016-05-20 22:57:42 -0700

    Display red border if user does not specify listing type in search

--- app/components/SearchListings.jsx
+++ app/components/SearchListings.jsx
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+
+import { searchListings } from '../actions/index';
 
 class SearchListings extends Component {
+    onSubmit(props) {
+        console.log(props)
+    }
+
     render() {
+        const { fields: { listing_type, search }, handleSubmit } = this.props;
+
         return (
             <div className='search-listings col-md-8 col-md-offset-2'>
-                <form>
-                    <div className='form-group'>
-                        <div className='row'>
-                            <div className='col-sm-2'>
-                                <select className="form-control">
+                <form onSubmit={ handleSubmit(this.onSubmit.bind(this)) }>
+                    <div className='row'>
+                        <div className='col-sm-2'>
+                            <div className={ `form-group ${ listing_type.touched && listing_type.invalid ? 'has-danger' : ''}` }>
+                                <select className='form-control' { ...listing_type }>
                                     <option value=''></option>
                                     <option value='All'>All</option>
                                     {/*<option value='Misc'>Misc</option>
@@ -17,12 +27,14 @@ class SearchListings extends Component {
                                     <option value='Car'>Car</option>*/}
                                 </select>
                             </div>
-                            <div className='col-sm-8'>
-                                <input disabled type='text' className='form-control' placeholder='Awesome things' />
+                        </div>
+                        <div className='col-sm-8'>
+                            <div className='form-group'>
+                                <input disabled type='text' className='form-control' placeholder='Awesome things' { ...search } />
                             </div>
-                            <div className='col-sm-2'>
-                                <button type='submit' className='btn btn-primary'>Search</button>
-                            </div>
+                        </div>
+                        <div className='col-sm-2'>
+                            <button type='submit' className='btn btn-primary'>Search</button>
                         </div>
                     </div>
                 </form>
@@ -31,4 +43,16 @@ class SearchListings extends Component {
     }
 }
 
-export default SearchListings;
+const validate = values => {
+    const errors = {};
+
+    if (!values.listing_type) errors.listing_type = 'Select a listing type';
+
+    return errors;
+};
+
+export default reduxForm({
+    form: 'SearchListingsForm',
+    fields: ['listing_type', 'search'],
+    validate
+}, null, { searchListings })(SearchListings);
